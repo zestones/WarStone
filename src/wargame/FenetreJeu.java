fa9e162c6e914a835b5c8bf24a8b4eb70176ee22
EINewstone <idrissbenguezzou@gmail.com>
2021-11-26 21:57:41 +0100

    clean: creation button & label moved --> PanneauJeu

--- src/wargame/FenetreJeu.java
+++ src/wargame/FenetreJeu.java
@@ -5,21 +5,16 @@ import javax.swing.*;
 
 public class FenetreJeu extends JFrame implements IConfig{
 	private static final long serialVersionUID = 1L;
-	public JLabel footer;
+	
 	FenetreJeu(){
 		// Creer la JFrame
 		JFrame frame = new JFrame("WarStone");	
 		frame.setSize(FEN_LARGEUR, FEN_HAUTEUR);	
 		frame.setLocation(POSITION_X, POSITION_Y);
 		
-		JMenuBar menuBar = new JMenuBar();
 		menuBar.setOpaque(true);
 		menuBar.setBackground(COULEUR_MENUBAR); 
 		menuBar.setPreferredSize(new Dimension(MENUBAR_LARGEUR, MENUBAR_HAUTEUR));
-		
-		JLabel label = new JLabel("Tout la Haut", SwingConstants.CENTER); 
-		label.setBackground(COULEUR_MENUBAR);
-		label.setOpaque(true); 
 		
 		frame.setBackground(COULEUR_INCONNU);
 		
@@ -29,25 +24,15 @@ public class FenetreJeu extends JFrame implements IConfig{
 		panel.setLayout(new BorderLayout());
 		panel.setOpaque(true);	    
 	
-		JLabel footer = new JLabel("", SwingConstants.CENTER); 
 		panel.add(footer, BorderLayout.SOUTH);
-		footer.setBackground(COULEUR_FOOTER);
-		footer.setPreferredSize(new Dimension(FOOTER_LARGEUR, FOOTER_HAUTEUR));
-		footer.setOpaque(true); //Test background
-	
+		
 		frame.setJMenuBar(menuBar);
 		
-		JButton finTour = new JButton("Fin du Tour");   
-		finTour.setSize(BOUTTON_LARGEUR, BOUTTON_HAUTEUR);
-		finTour.setVisible(true);
 		
-		panel.add(new PanneauJeu(finTour, footer, label));	
+		panel.add(new PanneauJeu());	
 		frame.getContentPane().add(panel);
         frame.add(panel);
         
-       
-        menuBar.add(finTour);
-        menuBar.add(label);
         menuBar.setLayout(new FlowLayout(FlowLayout.CENTER));        
         
         pack();
