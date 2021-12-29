8d002ea9ac22679f011fcda10b6b2756de54616e
EINewstone <idrissbenguezzou@gmail.com>
2021-12-29 02:24:44 +0100

    add: hover boutton for sauvegarde

--- src/menu/menupage/loadGameEvent.java
+++ src/menu/menupage/loadGameEvent.java
@@ -2,6 +2,8 @@ package menu.menupage;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import fenetrejeu.FenetreJeu;
 import menu.IMenu;
@@ -33,7 +35,28 @@ public class loadGameEvent implements ISauvegarde, IMenu {
 				}
 			});
 		}
+	
+		for(int i = 0; i < listeBoutton.size(); i++) {
+			listeBoutton.get(i).addMouseMotionListener(new MouseAdapter() {
+				public void mouseMoved(MouseEvent e) {
+					for(int j = 0; j < listeBoutton.size(); j++)
+						if(e.getSource() == listeBoutton.get(j)) {
+							listeBoutton.get(j).hoverBoutton();
+						}
+				}	
+			});
+		}
+		
+		
+		frame.addMouseMotionListener(new MouseAdapter() {
+			public void mouseMoved(MouseEvent e) {
+				for(int j = 0; j < listeBoutton.size(); j++) {
+					listeBoutton.get(j).unsetHoverBoutton();
+				}
+			}
+		});
 	}
+		
 	
 	private void removeBouton() {
 			for(Boutton save : listeBoutton)
